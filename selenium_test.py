04189c4d9b2b7de69e7a1216680529d34c83d385
Aashish Jangir <93608239+Aashishjangir112@users.noreply.github.com>
2024-04-05 14:12:11 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -13,7 +13,7 @@ driver.get("https://aashishjangir112.github.io/webpage")  # Update this with the
 time.sleep(2)  # Adding a delay to see the result
 
 # Assert some condition to verify the result
-assert "My Awesome Website" in driver.title
+assert "Welcome to My Simple Webpage" in driver.title
 
 # Take a screenshot
 timestamp = time.strftime("%Y%m%d-%H%M%S")
